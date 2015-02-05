2e6f55690ff197a1c451059ae30e8f89bf03658f
MCorentin <corentin.molitor@gmail.com>
2015-02-05 11:43:15 +0100

    Ajout UML

---
+++ Pichis/src/pichisNF/Observations.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pichisNF;
+
+import java.util.Date;
+
+/**
+ *
+ * @author molit_000
+ */
+public class Observations {
+    Date date;
+    String description;
+    
+    
+    public Observations(Date date, String description){
+        this.date = date;
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+}
