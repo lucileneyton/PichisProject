6dc74daa52c6aa768dda6d52b877b7a1b19c64ae
lucileneyton <lu.neyton@hotmail.fr>
2015-02-05 21:45:53 +0100

    Test connexion
    Test de connexion de la base de données avec essai de requête

---
+++ Bd/backup.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM pichisdatabase.dpi
+LIMIT 0, 1000
+
+-- Date: 2015-02-03 21:14
+*/
+INSERT INTO `dpi` (`ipp`,`nom`,`prenom`,`sexe`,`dateNaissance`,`adresse`,`ouvert`) VALUES ('123','test','test','H','2012-01-10','pifpafpouf','T');
