840693fb74d09f78b8f35966c52f579f55d8c124
MCorentin <corentin.molitor@gmail.com>
2015-02-05 09:11:48 +0100

    Mis en commun nom Packages

---
+++ Pichis/src/pichisNF/Administratif.java
@@ -0,0 +1,17 @@
+package pichisNF;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author molit_000
+ */
+public class Administratif extends Personnel{
+    
+    public Administratif(String id, String nom, String prenom, String motDePasse){
+        super(id, nom, prenom, motDePasse);
+    }
+}
