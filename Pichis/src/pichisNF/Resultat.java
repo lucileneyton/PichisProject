29fdc1cff3b539647501d1936972ce14acaa5db0
JohannE38 <johann.eymaron@gmail.com>
2015-03-26 21:07:30 +0100

    Javadoc5

--- Pichis/src/pichisNF/Resultat.java
+++ Pichis/src/pichisNF/Resultat.java
@@ -1,53 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pichisNF;
 
-import java.text.SimpleDateFormat;
-
-/**
- *
- * @author molit_000
- */
 public class Resultat {
-    DateSimple date;
-    String descriptions;
-    Medecin medecin;
-    
-    public Resultat(DateSimple date, String descriptions,Medecin medecin){
+
+    private DateSimple date;
+    private String descriptions;
+    private Medecin medecin;
+
+    /**
+     * Constructeur de la classe Résultat
+     *
+     * @param date
+     * @param descriptions
+     * @param medecin
+     */
+    public Resultat(DateSimple date, String descriptions, Medecin medecin) {
         this.date = date;
         this.descriptions = descriptions;
-        this.medecin=medecin;
-    }
-    
-    public void toSring(){
-        System.out.println(descriptions);
+        this.medecin = medecin;
     }
 
+    /**
+     * Méthode permettant l'accès au médecin ayant édité le résultat par
+     * d'autres classes
+     *
+     * @return Medecin
+     */
     public Medecin getMedecin() {
         return medecin;
     }
 
+    /**
+     * Méthode permettant l'accès à la date d'édition du résultat par d'autres
+     * classes
+     *
+     * @return DateSimple
+     */
     public DateSimple getDate() {
         return date;
     }
 
-    public void setDate(DateSimple date) {
-        this.date = date;
-    }
-
+    /**
+     * Méthode permettant l'accès à la description du résultat par d'autres
+     * classes
+     *
+     * @return String description
+     */
     public String getDescriptions() {
         return descriptions;
     }
 
-    public void setDescriptions(String descriptions) {
-        this.descriptions = descriptions;
-    }
-    
-    
-    
-    
-    
 }
--- Pichis/src/pichisNF/Sejour.java
+++ Pichis/src/pichisNF/Sejour.java
@@ -1,19 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pichisNF;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-
-/**
- *
- * @author molit_000
- */
 public class Sejour {
 
     private String numeroSejour;
@@ -23,10 +9,13 @@ public class Sejour {
     private Localisation localisation;
 
     /**
-     * constructeur de la classe sejour qui génère un numéro de séjour et qui ne
+     * Constructeur de la classe sejour qui génère un numéro de séjour et qui ne
      * possède pas de dateSortie
      *
-     *
+     * @param dma
+     * @param dateEntree
+     * @param PHResponsable
+     * @param localisation
      */
     public Sejour(DMA dma, DateSimple dateEntree, Medecin PHResponsable, Localisation localisation) {
         this.numeroSejour = fonctions.genererNumeroSejour(dateEntree);
@@ -36,13 +25,14 @@ public class Sejour {
         this.localisation = localisation;
     }
 
-    /** constructeur de la classe séjour qui ne possède pas de dateSortie
-     * 
+    /**
+     * Constructeur de la classe séjour qui ne possède pas de dateSortie
+     *
      * @param dma
      * @param numeroSejour
      * @param dateEntree
      * @param PHResponsable
-     * @param localisation 
+     * @param localisation
      */
     public Sejour(DMA dma, String numeroSejour, DateSimple dateEntree, Medecin PHResponsable, Localisation localisation) {
         this.numeroSejour = numeroSejour;
@@ -51,15 +41,16 @@ public class Sejour {
         this.PHResponsable = PHResponsable;
         this.localisation = localisation;
     }
-    
-    /** constructeur de la classe séjour qui prend un numéroSéjour et dateSortie
-     * 
+
+    /**
+     * Constructeur de la classe séjour qui prend un numéroSéjour et dateSortie
+     *
      * @param dma
      * @param numeroSejour
      * @param dateEntree
      * @param dateSortie
      * @param PHResponsable
-     * @param localisation 
+     * @param localisation
      */
     public Sejour(DMA dma, String numeroSejour, DateSimple dateEntree, DateSimple dateSortie, Medecin PHResponsable, Localisation localisation) {
         this.numeroSejour = numeroSejour;
@@ -69,13 +60,15 @@ public class Sejour {
         this.localisation = localisation;
     }
 
-    /** constructeur de la classe séjour qui prend un numéro de séjour et une date de sortie
-     * 
+    /**
+     * Constructeur de la classe séjour qui prend un numéro de séjour et une
+     * date de sortie
+     *
      * @param dma
      * @param dateEntree
      * @param dateSortie
      * @param PHResponsable
-     * @param localisation 
+     * @param localisation
      */
     public Sejour(DMA dma, DateSimple dateEntree, DateSimple dateSortie, Medecin PHResponsable, Localisation localisation) {
         this.numeroSejour = fonctions.genererNumeroSejour(dateEntree);
@@ -86,69 +79,65 @@ public class Sejour {
         this.localisation = localisation;
     }
 
-    /** méthode permettant l'accès de la date d'entrée aux autres classes
-     * 
-     * 
+    /**
+     * Méthode permettant l'accès de la date d'entrée par d'autres classes
+     *
+     * @return DateSimple dateEntree
      */
     public DateSimple getDateEntree() {
         return dateEntree;
     }
 
-    public void setDateEntree(DateSimple dateEntree) {
-        this.dateEntree = dateEntree;
-    }
-    
-    /** méthode permettant l'accès de la date de sortie aux autres classes
-     * 
-     * 
+    /**
+     * Méthode permettant l'accès de la date de sortie par d'autres classes
+     *
+     * @return DateSimple dateSortie
      */
     public DateSimple getDateSortie() {
         return dateSortie;
     }
 
-    public void setDateSortie(DateSimple dateSortie) {
-        this.dateSortie = dateSortie;
-    }
-
-    /** méthode permettant l'accès au numéro de séjour aux autres classes
-     * 
-     * 
+    /**
+     * Méthode permettant l'accès au numéro de séjour par d'autres classes
+     *
+     * @return String numeroSejour
      */
     public String getNumeroSejour() {
         return numeroSejour;
     }
 
-    public void setNumeroSejour(String numeroSejour) {
-        this.numeroSejour = numeroSejour;
-    }
-
-    /** méthode permettant l'accès au PH responsable aux autres classes
-     * 
-     * 
+    /**
+     * Méthode permettant l'accès au PH responsable par d'autres classes
+     *
+     * @return Medecin
      */
     public Medecin getPHResponsable() {
         return PHResponsable;
     }
 
-    public void setPHResponsable(Medecin PHResponsable) {
-        this.PHResponsable = PHResponsable;
-    }
-    
-    /** méthode permettant l'accès de la localisation aux autres classes
-     * 
-     * 
+    /**
+     * Méthode permettant l'accès de la localisation aux autres classes
+     *
+     * @return Localisation
      */
     public Localisation getLocalisation() {
         return localisation;
     }
 
+    /**
+     * Méthode permettant la modification de la localisation du patient dans son
+     * séjour
+     *
+     * @param localisation
+     */
     public void setLocalisation(Localisation localisation) {
         this.localisation = localisation;
     }
 
-    /** méthode qui renvoie le numéro de séjour sous version texte
-     * 
-     * 
+    /**
+     * Méthode permettant l'affichage du numéro de séjour
+     *
+     * @return String numeroSejour
      */
     public String toString() {
         return this.numeroSejour;
