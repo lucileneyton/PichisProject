1c57e46e9760a56b3900dee82c77262406f580ae
JohannE38 <johann.eymaron@gmail.com>
2015-03-26 19:15:09 +0100

    javadoc4

--- Pichis/src/pichisNF/Prescription.java
+++ Pichis/src/pichisNF/Prescription.java
@@ -1,56 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pichisNF;
 
-import java.text.SimpleDateFormat;
-
-/**
- *
- * @author molit_000
- */
 public class Prescription {
-    DateSimple date;
-    String prescriptions;
-    Medecin medecin;
-    
-    public Prescription(){
-    } 
-    
-    public Prescription(DateSimple date, String prescriptions, Medecin medecin){
+
+    private DateSimple date;
+    private String prescriptions;
+    private Medecin medecin;
+
+    /**
+     * Constructeur de la classe Prescription
+     *
+     * @param date
+     * @param prescriptions
+     * @param medecin
+     */
+    public Prescription(DateSimple date, String prescriptions, Medecin medecin) {
         this.date = date;
         this.prescriptions = prescriptions;
         this.medecin = medecin;
     }
 
+    /**
+     * Méthode permettant l'accès à la date de la prescrition par d'autres
+     * classes
+     *
+     * @return DateSimple
+     */
     public DateSimple getDate() {
         return date;
     }
 
-    public void setDate(DateSimple date) {
-        this.date = date;
-    }
-
+    /**
+     * Méthode permettant l'accès à la description de la prescription par
+     * d'autres classes
+     *
+     * @return String prescription
+     */
     public String getPrescriptions() {
         return prescriptions;
     }
 
+    /**
+     * Méthode permettant l'accès au medecin ayant fait la prescription par
+     * d'autres classes
+     *
+     * @return Medecin
+     */
     public Medecin getMedecin() {
         return medecin;
     }
 
-    public void setPrescriptions(String prescriptions) {
-        this.prescriptions = prescriptions;
-    }
-    
-    public String toString(){
-//        return "----- Prescription -----"
-//                + "\ndate : " + this.date.toString()
-//                +"\nprescription : " + this.prescriptions
-//                +"\nmedecin : " +this.medecin.toString();
+    /**
+     * Méthode permettant l'affichage des informations de la prescription avec
+     * la date d'édition dans les listes de l'interface
+     *
+     * @return String prescription
+     */
+    public String toString() {
         return "Editée le " + this.date.toString();
     }
-    
+
 }
--- Pichis/src/pichisNF/Prestations.java
+++ Pichis/src/pichisNF/Prestations.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pichisNF;
 
-import java.util.Calendar;
-
-/**
- *
- * @author Johann
- */
 public class Prestations implements Comparable {
 
     private String naturePrestation;
@@ -20,6 +9,17 @@ public class Prestations implements Comparable {
     private DateSimple date;
     private String idPrestation;
 
+    /**
+     * Premier constructeur de la classe Prestations prenant en compte l'id de
+     * la prestation
+     *
+     * @param naturePrestation
+     * @param demandeur
+     * @param patient
+     * @param resultat
+     * @param date
+     * @param idPrestation
+     */
     public Prestations(String naturePrestation, Medecin demandeur, DPI patient, Resultat resultat, DateSimple date, String idPrestation) {
         this.naturePrestation = naturePrestation;
         this.demandeur = demandeur;
@@ -29,6 +29,15 @@ public class Prestations implements Comparable {
         this.idPrestation = idPrestation;
     }
 
+    /**
+     * Deuxième constructeur ne prenant pas en compte l'id de la prestation
+     *
+     * @param naturePrestation
+     * @param demandeur
+     * @param patient
+     * @param resultat
+     * @param date
+     */
     public Prestations(String naturePrestation, Medecin demandeur, DPI patient, Resultat resultat, DateSimple date) {
         this.naturePrestation = naturePrestation;
         this.demandeur = demandeur;
@@ -37,45 +46,97 @@ public class Prestations implements Comparable {
         this.date = date;
     }
 
+    /**
+     * Méthode permettant l'accès au dossier patient informatisé (dpi) du
+     * patient par d'autres classes
+     *
+     * @return DPI
+     */
     public DPI getPatient() {
         return patient;
     }
 
+    /**
+     * Méthode permettant l'accès à la nature de la prestation par d'autres
+     * classes
+     *
+     * @return String naturePrestation
+     */
     public String getNaturePrestation() {
         return naturePrestation;
     }
 
+    /**
+     * Méthode permettant l'accès au médein qui a demandé la prestation par
+     * d'autres classes
+     *
+     * @return Medecin demandeur
+     */
     public Medecin getDemandeur() {
         return demandeur;
     }
 
+    /**
+     * Méthode permettant l'accès au résultat de la prestation par d'autres
+     * classes
+     *
+     * @return Resultat
+     */
     public Resultat getResultat() {
         return resultat;
     }
 
+    /**
+     * Méthode permettant la modification du résultat (initialement égal à null)
+     * après réalisation de la prestation
+     *
+     * @param resultat
+     */
     public void setResultat(Resultat resultat) {
         this.resultat = resultat;
     }
 
+    /**
+     * Méthode permettant l'accès à la date de demande de la prestation par
+     * d'autres classes
+     *
+     * @return DateSimple
+     */
     public DateSimple getDate() {
         return date;
     }
 
+    /**
+     * Méthode permettant l'accès à l'id de la prestation par d'autres classes
+     *
+     * @return idPrestation
+     */
+    public String getIdPrestation() {
+        return idPrestation;
+    }
+
+    /**
+     * Méthode permettant l'affichage de la prestation avec sa date de demande
+     * et sa nature dans les listes de l'interface
+     *
+     * @return String prestation
+     */
     public String toString() {
         return "Demande du " + date.toString() + " / Prestation : " + this.naturePrestation;
     }
 
+    /**
+     * Methode permettant de classer les prestations par date
+     *
+     * @param o
+     * @return int
+     */
     public int compareTo(Object o) {
         Prestations p = (Prestations) o;
         DateSimple d = p.getDate();
         int resultat = this.date.compareTo(d);
 
         return resultat;
-
-    }
-
-    public String getIdPrestation() {
-        return idPrestation;
     }
 
 }
