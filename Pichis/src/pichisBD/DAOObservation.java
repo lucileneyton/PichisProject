722d86e81678c8f86b37dcc035c1aa775ca64ac7
lucileneyton <lu.neyton@hotmail.fr>
2015-03-26 15:31:16 +0100

    MAJ

--- Pichis/src/pichisBD/DAOObservation.java
+++ Pichis/src/pichisBD/DAOObservation.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pichisBD;
 
 import java.sql.ResultSet;
@@ -20,9 +19,18 @@ import pichisNF.DPI;
  * @author Lucile
  */
 public class DAOObservation {
-    
-    ConnectionBD c;
-    
+
+    public ConnectionBD c;
+
+    /**
+     * Méthode permettant d'ajouter une observation dans la base de données
+     *
+     * @param id l'identifiant du personnel
+     * @param idMedecin l'identifiant du médecin ayant rédigé l'observation
+     * @param texte le contenu de l'observation
+     * @param patient l'ipp du patient concerné
+     * @param date la date d'ajout de l'observation
+     */
     public void ajoutObservation(String id, String idMedecin, String texte, String patient, String date) {
         Statement ins;
         c = new ConnectionBD();
@@ -44,18 +52,22 @@ public class DAOObservation {
 
         }
     }
-    
+
+    /**
+     * Méthode retournant les observations d'un patient
+     *
+     * @param idPatient l'identifiant du patient
+     * @return ArrayList
+     */
     public ArrayList<Observation> observationsPatient(String idPatient) {
-        
-        
+
         c = new ConnectionBD();
         ArrayList<Observation> liste = new ArrayList<Observation>();
         String date;
         String m;
         String texte;
         String p;
-        
-        
+
         try {
 
             ResultSet resul;
@@ -69,16 +81,16 @@ public class DAOObservation {
                 m = resul.getString("medecin");
                 DAOMedecin daom = new DAOMedecin();
                 Medecin med = daom.medecinParID(m);
-                
+
                 texte = resul.getString("texte");
-                
+
                 p = resul.getString("patient");
                 DAODPI daop = new DAODPI();
                 DPI dpi = daop.dpiParIPP(p);
-                
+
                 date = resul.getString("date");
-                
-                DateSimple dateRenvoyee =null;
+
+                DateSimple dateRenvoyee = null;
                 if (date != null) {
                     if (date.length() <= 11) {
                         dateRenvoyee = new DateSimple(date.substring(0, 2), date.substring(3, 5), date.substring(6, 10));
@@ -86,14 +98,14 @@ public class DAOObservation {
                         dateRenvoyee = new DateSimple(date.substring(0, 2), date.substring(3, 5), date.substring(6, 10), date.substring(13, 15), date.substring(16, 18));
                     }
                 }
-                Observation o = new Observation(dateRenvoyee,texte,med);
+                Observation o = new Observation(dateRenvoyee, texte, med);
 
                 liste.add(o);
-                
+
             }
 
         } catch (SQLException e) {
-            System.out.println("erreur DAOObservation: " + e);
+            System.out.println("erreur DAOObservation(observationsPatient): " + e);
         } finally {
             if (c != null) {
                 try {
